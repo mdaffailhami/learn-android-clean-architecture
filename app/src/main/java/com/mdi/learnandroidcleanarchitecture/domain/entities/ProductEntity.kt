d56495a9a6411da866ea164a8046219fe8fd8846
Daffa Ilhami <mdaffailhami@gmail.com>
2024-10-03 08:29:46 +0700

    Add fromProductModel factory in ProductEntity

--- app/src/main/java/com/mdi/learnandroidcleanarchitecture/domain/entities/ProductEntity.kt
+++ app/src/main/java/com/mdi/learnandroidcleanarchitecture/domain/entities/ProductEntity.kt
@@ -1,9 +1,23 @@
 package com.mdi.learnandroidcleanarchitecture.domain.entities
 
+import com.mdi.learnandroidcleanarchitecture.data.models.ProductModel
+
 data class ProductEntity(
     var title: String,
     var price: Double,
     var description: String,
     var image: String,
     var rating: Double
-)
+) {
+    companion object {
+        fun fromProductModel(productModel: ProductModel): ProductEntity {
+            return ProductEntity(
+                title = productModel.title,
+                price = productModel.price,
+                description = productModel.description,
+                image = productModel.image,
+                rating = productModel.rating.rate
+            )
+        }
+    }
+}
